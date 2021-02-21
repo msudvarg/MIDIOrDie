5674aead70362d6c6b33202603fa23cbf472c99b
Marion Sudvarg <msudvarg@gmail.com>
2021-02-21 12:48:55 -0800

    Updated Python visualize script to tune skipping data points for performance

--- controller/visualize.py
+++ controller/visualize.py
@@ -2,11 +2,19 @@ import fft_histogram
 import matplotlib
 import matplotlib.pyplot as plt
 
+scale = int(20) #Only plot 1 out of every scale datapoints
+
 plt.show()
 
 while 1:
-    y = fft_histogram.histogram()
-    x = range(len(y))
+    y_full = fft_histogram.histogram()
+    x = range(int(len(y_full)/scale))
+    y = []
+    if scale > 1:
+        for i in x:
+            y.append(y_full[i*scale])
+    else:
+        y = y_full
     plt.cla()
     plt.plot(x,y)
     plt.draw()
