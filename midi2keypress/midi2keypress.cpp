3b3c45bc7dcf2e5bd3d8079a2042a1dd700a4e95
Dane Johnson <dane_johnson@ymail.com>
2021-03-17 13:10:29 -0500

    Send all frequencies or the first peak at your option

--- midi2keypress/midi2keypress.cpp
+++ midi2keypress/midi2keypress.cpp
@@ -8,7 +8,18 @@
 
 bool done = false;
 
-int main() {
+int main(int argc, char *argv[]) {
+  int opt;
+  bool all;
+  
+  while((opt = getopt(argc, argv, "a")) != -1) {
+    switch(opt) {
+    case 'a':
+      all = true;
+      break;
+    }
+  }
+  
   LocalController lc;
   MidiStream ms;
   ms.Init();
@@ -21,18 +32,41 @@ int main() {
 
     FreqList peaks = tone.GetPeakPitches();
 
-    for (int freq : FreqDifference(peaks, old_peaks)) {
-      if (InMidiRange(freq)) {
-	ms.Send(Freq2Midi(freq), true);
+    if (all) {
+      // Play all the pitches found in the fft
+      for (int freq : FreqDifference(peaks, old_peaks)) {
+	if (InMidiRange(freq)) {
+	  ms.Send(Freq2Midi(freq), true);
+	}
+      }
+
+      for (int freq : FreqDifference(old_peaks, peaks)) {
+	if (InMidiRange(freq)) {
+	  ms.Send(Freq2Midi(freq), false);
+	}
+      }
+    } else {
+      // Just play the fundamental frequency
+      if (peaks.size() > 0) {
+	int freq = peaks[0];
+	if (old_peaks.size() == 0) {
+	  if (InMidiRange(freq)) {
+	    ms.Send(Freq2Midi(freq), true);
+	  }
+	} else if (old_peaks[0] != freq) {
+	  if (InMidiRange(freq)) {
+	    ms.Send(Freq2Midi(freq), true);
+	  }
+	  if (InMidiRange(old_peaks[0])) {
+	    ms.Send(Freq2Midi(old_peaks[0]), false);
+	  }
+	}
+      } else if (old_peaks.size() > 0) {
+	if (InMidiRange(old_peaks[0])) {
+	  ms.Send(Freq2Midi(old_peaks[0]), false);
+	}
       }
     }
-
-    for (int freq : FreqDifference(old_peaks, peaks)) {
-      if (InMidiRange(freq)) {
-	ms.Send(Freq2Midi(freq), false);
-      }
-    }
-
     old_peaks = peaks;
   }
 }
