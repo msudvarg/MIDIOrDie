25105bdc09ad6c9120e3c6088a71b095d5fe9bbc
Marion Sudvarg <msudvarg@gmail.com>
2021-04-04 19:07:39 -0700

    channelbroker test is a little nicer

--- testprograms/channelbroker_test.cpp
+++ testprograms/channelbroker_test.cpp
@@ -6,38 +6,48 @@
 #include "../fft2midi/channelbroker.h"
 
 ChannelBroker channel_broker {false}; //Do not print ports
-std::atomic<int> thread_counter;
 std::mutex cout_mtx;
 
-void test(int thread_count, bool drum) {
-    thread_counter++;
-    while(thread_counter < thread_count) {}
+//Barrier implementation is dumb but it works
+std::atomic<int> b1;
+std::atomic<int> b2;
+void barrier(std::atomic<int> & b) {
+    b++;
+    while (b < 4) {}
+}
+
+void test(bool drum) {
+    barrier(b1);
     Channel channel (channel_broker, drum);
     cout_mtx.lock();
+    //std::cout << "I am thread " << local_counter << ". ";
     if(drum) std::cout << "I asked for the drum. ";
     std::cout << "I got channel " << channel.get_channel() << std::endl;
     cout_mtx.unlock();
+    barrier(b2);
+}
+
+void make_threads(void) {
+    b1 = 0;
+    b2 = 0;
+    std::vector<std::thread> threads;
+    
+    threads.emplace_back(test, true);
+    threads.emplace_back(test, false);
+    threads.emplace_back(test, true);
+    threads.emplace_back(test, false);
+    
+    for (std::thread & t : threads) {
+        if(t.joinable()) t.join();
+    }
 }
 
 
 int main(int argc, char * argv[]) {
 
-    thread_counter = 0;
-    std::vector<std::thread> threads;
-    threads.emplace_back(test, 4, true);
-    threads.emplace_back(test, 4, false);
-    threads.emplace_back(test, 4, true);
-    threads.emplace_back(test, 4, false);
-    while (thread_counter < 4) {}
-    threads.emplace_back(test, 8, true);
-    threads.emplace_back(test, 8, false);
-    threads.emplace_back(test, 8, true);
-    threads.emplace_back(test, 8, false);
-    for (std::thread & t : threads) {
-        if(t.joinable()) t.join();
-    }
+    make_threads();
+    make_threads();
 
-
-
+    return 0;
 
 }
