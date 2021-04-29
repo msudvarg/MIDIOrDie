f00285674b08bbd837951e91f6bc0622c155bd7f
msudvarg <msudvarg@gmail.com>
2021-04-29 13:51:05 -0500

    install_controller now includes apt-get update

--- install_controller.sh
+++ install_controller.sh
@@ -3,6 +3,7 @@ git submodule init
 git submodule update
 
 #Install dependencies on Debian-based system (e.g. Raspberry Pi)
+sudo apt-get update
 sudo apt-get install cmake jack libfftw3-dev librtmidi-dev portaudio19-dev
 
 mkdir build
