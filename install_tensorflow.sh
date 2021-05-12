94502ee0eecee9e593cae45fb5d966e81c6c81f5
Marion Sudvarg <msudvarg@gmail.com>
2021-05-12 11:15:22 -0500

    Must apt-get update to install bazel

--- install_tensorflow.sh
+++ install_tensorflow.sh
@@ -5,6 +5,7 @@ cd build
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt-get update
 sudo apt-get install bazel-3.1.0
 sudo ln -s /usr/bin/bazel-3.1.0 /usr/bin/bazel
 
